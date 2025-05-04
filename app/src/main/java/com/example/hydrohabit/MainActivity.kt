d35d0715cdd99d94cfa061727887e06c0c81ed82
GitterHubber21 <andrashorvath300@gmail.com>
2025-05-04 11:46:49 +0200

    Foundation

--- app/src/main/java/com/example/hydrohabit/MainActivity.kt
+++ app/src/main/java/com/example/hydrohabit/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.hydrohabit
 
 import android.app.Activity
-import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
+
 
 class MainActivity : Activity() {
 
