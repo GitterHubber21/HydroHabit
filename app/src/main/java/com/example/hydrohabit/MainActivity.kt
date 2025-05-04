a5479ef9ade4853cd847dbfe6e7a3689cc2e48e7
GitterHubber21 <andrashorvath300@gmail.com>
2025-05-04 11:43:53 +0200

    Foundation

--- app/src/main/java/com/example/hydrohabit/MainActivity.kt
+++ app/src/main/java/com/example/hydrohabit/MainActivity.kt
@@ -1,47 +1,17 @@
 package com.example.hydrohabit
 
+import android.app.Activity
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hydrohabit.ui.theme.HydroHabitTheme
+import android.widget.Button
+import android.widget.ImageView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            HydroHabitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.home)
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HydroHabitTheme {
-        Greeting("Android")
-    }
-}
