0fa34bc93ef2fb618f77f4bf31eab742c66eaa9b
GitterHubber21 <andrashorvath300@gmail.com>
2025-06-04 13:49:34 +0200

    Modified main.py

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,14 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-data_store = {"quantity": "100ml"}
 
-@app.route('/api/quantity', methods=['GET'])
-def get_value():
-    return jsonify(data_store)
+@app.route('/api/hello')
+def hello():
+    return jsonify(message="Hello from Flask!")
 
-@app.route('/api/quantity', methods=['POST'])
-def update_value():
-    content = request.get_json()
-    key = content.get("key")
-    value = content.get("value")
-
-    if key in data_store:
-        data_store[key] = value
-        return jsonify({"message": "Value updated", "data": data_store}), 200
-    else:
-        return jsonify({"error": "Key not found"}), 404
-
+@app.route('/api/color')
+def color():
+    return jsonify(color="Yellow!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=False)
